eb47b73b5ad3d849b9873230728caf970b4a7dae
Darrell Conklin <dmconklin@Darrells-MacBook-Pro.local>
2024-06-15 00:19:40 -0500

    Completed Task 2

--- frontend/index.js
+++ frontend/index.js
@@ -10,7 +10,13 @@ function moduleProject1() {
   document.querySelectorAll('section div').forEach(widget => widget.classList.add('widget'))
   // 👉 TASK 2 - Build a "Quote of the Day" widget
   //  ✨ add your code here
-
+  let random = Math.floor(Math.random() * quotes.length)
+  let quoteDiv = document.createElement('div')
+  quoteDiv.textContent = quotes[random]['quote']
+  let authorDiv = document.createElement('div')
+  authorDiv.textContent = quotes[random]['author'] + ' in ' + (quotes[random]['date'] === null ? 'an unknown date' : quotes[random]['date'])
+  document.querySelector('.quoteoftheday').append(quoteDiv)
+  document.querySelector('.quoteoftheday').append(authorDiv)
   // 👉 TASK 3 - Build a "Corporate Speak" widget
   //  ✨ add your code here
 
